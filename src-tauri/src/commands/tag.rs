b3a7921d5707a08b2bc63597207553a01cdc8301
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2025-01-02 17:47:25 +0900

    create_tag_in_dbを実装

--- src-tauri/src/commands/tag.rs
+++ src-tauri/src/commands/tag.rs
@@ -1,1 +1,10 @@
+use sqlx::{Pool, Sqlite};
 
+pub async fn create_tag_in_db(sqlite_pool: Pool<Sqlite>, name: String) -> Result<i64, ()> {
+    let tag = sqlx::query("INSERT INTO Tags (name) VALUES (?)")
+        .bind(name)
+        .execute(&sqlite_pool)
+        .await
+        .unwrap();
+    Ok(tag.rows_affected() as i64)
+}
