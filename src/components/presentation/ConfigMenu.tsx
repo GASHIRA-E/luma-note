8b34f99046d470d8066766d3ba3f73ca30b219ce
GASHIRA-E <dsteams@outlook.jp>
2025-01-03 19:41:20 +0900

    設定のタグ削除確認モーダル実装

--- src/components/presentation/ConfigMenu.tsx
+++ src/components/presentation/ConfigMenu.tsx
@@ -11,6 +11,8 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { SegmentedControl } from "@/components/ui/segmented-control";
+
+import { DeleteItemConfirmDialog } from "@/components/parts/DeleteItemConfirmDialog";
 import { AppThemes } from "@/utils/constants";
 
 export type ConfigMenuProps = {
@@ -18,6 +20,9 @@ export type ConfigMenuProps = {
   allTags: string[];
   onChangeTheme: (theme: string) => void;
   onClickRemoveTag: (tag: string) => void;
+  deleteItemConfigDialogProps: React.ComponentProps<
+    typeof DeleteItemConfirmDialog
+  >;
 };
 
 export const ConfigMenu = ({
@@ -25,45 +30,51 @@ export const ConfigMenu = ({
   allTags,
   onChangeTheme,
   onClickRemoveTag,
+  deleteItemConfigDialogProps,
 }: ConfigMenuProps) => {
   return (
-    <DrawerRoot>
-      <DrawerTrigger asChild>
-        <Button onClick={(event) => event.currentTarget.blur()}>Config</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>設定</DrawerTitle>
-        </DrawerHeader>
-        <DrawerBody>
-          <Box mb={4}>
-            {/* テーマ切り替え */}
-            <Text mb={2}>アプリテーマ切り替え</Text>
-            <SegmentedControl
-              onValueChange={(e) => onChangeTheme(e.value)}
-              defaultValue={currentTheme}
-              items={Object.values(AppThemes)}
-            />
-          </Box>
-          {/* タグの修正 */}
-          <Text mb={2}>タグ一覧</Text>
-          <VStack
-            alignItems="start"
-            maxH="40"
-            overflowY="scroll"
-            bg="bg.subtle"
-            p={2}
-          >
-            {allTags.map((tag, i) => (
-              <Tag key={i} closable onClose={() => onClickRemoveTag(tag)}>
-                {tag}
-              </Tag>
-            ))}
-          </VStack>
-        </DrawerBody>
-        <DrawerCloseTrigger />
-      </DrawerContent>
-      <DrawerBackdrop />
-    </DrawerRoot>
+    <>
+      <DrawerRoot>
+        <DrawerTrigger asChild>
+          <Button onClick={(event) => event.currentTarget.blur()}>
+            Config
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>設定</DrawerTitle>
+          </DrawerHeader>
+          <DrawerBody>
+            <Box mb={4}>
+              {/* テーマ切り替え */}
+              <Text mb={2}>アプリテーマ切り替え</Text>
+              <SegmentedControl
+                onValueChange={(e) => onChangeTheme(e.value)}
+                defaultValue={currentTheme}
+                items={Object.values(AppThemes)}
+              />
+            </Box>
+            {/* タグの修正 */}
+            <Text mb={2}>タグ一覧</Text>
+            <VStack
+              alignItems="start"
+              maxH="40"
+              overflowY="scroll"
+              bg="bg.subtle"
+              p={2}
+            >
+              {allTags.map((tag, i) => (
+                <Tag key={i} closable onClose={() => onClickRemoveTag(tag)}>
+                  {tag}
+                </Tag>
+              ))}
+            </VStack>
+          </DrawerBody>
+          <DrawerCloseTrigger />
+        </DrawerContent>
+        <DrawerBackdrop />
+      </DrawerRoot>
+      <DeleteItemConfirmDialog {...deleteItemConfigDialogProps} />
+    </>
   );
 };
