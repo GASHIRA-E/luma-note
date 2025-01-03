550cda6d913c5526efe6b0a4c6718b6766623e52
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2025-01-03 18:36:26 +0900

    find_memo実装完了

--- src-tauri/src/commands/search.rs
+++ src-tauri/src/commands/search.rs
@@ -11,14 +11,14 @@ pub async fn find_memo(
     Ok(memos)
 }
 
+/// title: 部分一致
+/// tags: ORにてひとつでも含まれている場合
+/// title と tags は AND にて検索される
 async fn find_memo_from_db(
     sqlite_pool: Pool<Sqlite>,
     memo_title: String,
     tags: Vec<i32>,
 ) -> Result<Vec<DetailMemoInfo>, ()> {
-    // メモタイトルとタグIDを結合して検索する
-    // メモタイトルはLIKE検索
-    // タグIDはIN検索
     let sql = format!(
         r#"
         SELECT DISTINCT
@@ -35,17 +35,34 @@ async fn find_memo_from_db(
             ) as tags
         FROM Memos m
         LEFT JOIN MemoTagRelations mt ON m.id = mt.memo_id
-        WHERE m.title LIKE ?
+        LEFT JOIN Tags t ON mt.tag_id = t.id
+        WHERE 1=1
         {}
+        {}
+        GROUP BY m.id
+        ORDER BY m.id
         "#,
+        if memo_title.is_empty() {
+            ""
+        } else {
+            "AND m.title LIKE ?"
+        },
         if tags.is_empty() {
             ""
         } else {
-            "AND mt.tag_id IN (SELECT value FROM json_each(?))"
+            "AND m.id IN (
+                SELECT DISTINCT memo_id 
+                FROM MemoTagRelations 
+                WHERE tag_id IN (SELECT value FROM json_each(?))
+            )"
         }
     );
 
-    let mut query = sqlx::query_as::<_, RawDetailMemo>(&sql).bind(format!("%{}%", memo_title));
+    let mut query = sqlx::query_as::<_, RawDetailMemo>(&sql);
+
+    if !memo_title.is_empty() {
+        query = query.bind(format!("%{}%", memo_title));
+    }
 
     if !tags.is_empty() {
         query = query.bind(serde_json::to_string(&tags).unwrap());
@@ -78,122 +95,162 @@ mod tests {
     use crate::commands::tag::create_tag_in_db;
     use crate::database::setup_test_db;
     use crate::types::CreateMemoIn;
-
-    #[tokio::test]
-    async fn test_メモを検索できること() {
-        let sqlite_pool = setup_test_db().await;
+    #[cfg(test)]
+    async fn setup_test_data(sqlite_pool: Pool<Sqlite>) -> Vec<TagInfo> {
+        //[
+        //    {
+        //     id: 1,
+        //     title: "abc",
+        //     tags: ["rust", "typescript", "javascript"],
+        //   },
+        //   {
+        //     id: 2,
+        //     title: "abcd",
+        //     tags: ["rust", "typescript"],
+        //   },
+        //   {
+        //     id: 3,
+        //     title: "abcde",
+        //     tags: [],
+        //   },
+        //   {
+        //     id: 4,
+        //     title: "abcdef",
+        //     tags: ["typescript"],
+        //   }
+        // ]
 
         create_folder_in_db(sqlite_pool.clone(), "test".to_string())
             .await
             .unwrap();
 
-        create_tag_in_db(sqlite_pool.clone(), "tag1".to_string())
+        let rust_tag_id = create_tag_in_db(sqlite_pool.clone(), "rust".to_string())
             .await
             .unwrap();
-        create_tag_in_db(sqlite_pool.clone(), "tag2".to_string())
+        let typescript_tag_id = create_tag_in_db(sqlite_pool.clone(), "typescript".to_string())
             .await
             .unwrap();
 
-        let memo = CreateMemoIn {
-            title: "test".to_string(),
-            folder_id: Some(1),
-            content: "test".to_string(),
-            tags: Some(vec![1, 2]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo).await.unwrap();
-
-        let memos = find_memo_from_db(sqlite_pool.clone(), "test".to_string(), vec![1, 2])
+        let javascript_tag_id = create_tag_in_db(sqlite_pool.clone(), "javascript".to_string())
             .await
             .unwrap();
-        assert_eq!(memos.len(), 1);
+
+        create_memo_in_db(
+            sqlite_pool.clone(),
+            CreateMemoIn {
+                title: "abc".to_string(),
+                folder_id: Some(1),
+                content: "test".to_string(),
+                tags: Some(vec![rust_tag_id, typescript_tag_id, javascript_tag_id]),
+            },
+        )
+        .await
+        .unwrap();
+
+        create_memo_in_db(
+            sqlite_pool.clone(),
+            CreateMemoIn {
+                title: "abcd".to_string(),
+                folder_id: Some(1),
+                content: "test".to_string(),
+                tags: Some(vec![rust_tag_id, typescript_tag_id]),
+            },
+        )
+        .await
+        .unwrap();
+
+        create_memo_in_db(
+            sqlite_pool.clone(),
+            CreateMemoIn {
+                title: "abcde".to_string(),
+                folder_id: Some(1),
+                content: "test".to_string(),
+                tags: Some(vec![]),
+            },
+        )
+        .await
+        .unwrap();
+
+        create_memo_in_db(
+            sqlite_pool.clone(),
+            CreateMemoIn {
+                title: "abcdef".to_string(),
+                folder_id: Some(1),
+                content: "test".to_string(),
+                tags: Some(vec![typescript_tag_id]),
+            },
+        )
+        .await
+        .unwrap();
+
+        vec![
+            TagInfo {
+                id: rust_tag_id,
+                name: "rust".to_string(),
+            },
+            TagInfo {
+                id: typescript_tag_id,
+                name: "typescript".to_string(),
+            },
+            TagInfo {
+                id: javascript_tag_id,
+                name: "javascript".to_string(),
+            },
+        ]
+    }
+
+    #[tokio::test]
+    async fn test_タイトルとタグの両方で検索できること() {
+        let sqlite_pool = setup_test_db().await;
+        let tags = setup_test_data(sqlite_pool.clone()).await;
+
+        // tags = [1:rust, 2:typescript]
+        // title = "abc"
+        let memos = find_memo_from_db(
+            sqlite_pool.clone(),
+            "abc".to_string(),
+            vec![tags[0].id as i32, tags[1].id as i32],
+        )
+        .await
+        .unwrap();
+        assert_eq!(memos.len(), 3);
+        assert_eq!(memos[0].id, 1);
+        assert_eq!(memos[1].id, 2);
+        assert_eq!(memos[2].id, 4);
     }
 
     #[tokio::test]
     async fn test_タグのみ検索ができること() {
         let sqlite_pool = setup_test_db().await;
+        let tags = setup_test_data(sqlite_pool.clone()).await;
 
-        create_folder_in_db(sqlite_pool.clone(), "test".to_string())
-            .await
-            .unwrap();
-
-        create_tag_in_db(sqlite_pool.clone(), "tag1".to_string())
-            .await
-            .unwrap();
-        create_tag_in_db(sqlite_pool.clone(), "tag2".to_string())
-            .await
-            .unwrap();
-
-        let memo = CreateMemoIn {
-            title: "test".to_string(),
-            folder_id: Some(1),
-            content: "test".to_string(),
-            tags: Some(vec![1]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo).await.unwrap();
-
-        let memo2 = CreateMemoIn {
-            title: "test2".to_string(),
-            folder_id: Some(1),
-            content: "test2".to_string(),
-            tags: Some(vec![2]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo2).await.unwrap();
-
-        let memos = find_memo_from_db(sqlite_pool.clone(), "".to_string(), vec![1, 2])
-            .await
-            .unwrap();
-        assert_eq!(memos.len(), 2);
-        assert_eq!(
-            memos[0].tags,
-            Some(vec![TagInfo {
-                id: 1,
-                name: "tag1".to_string()
-            }])
-        );
-        assert_eq!(
-            memos[1].tags,
-            Some(vec![TagInfo {
-                id: 2,
-                name: "tag2".to_string()
-            }])
-        );
+        // tags = [2:typescript, 3:javascript]
+        // title = ""
+        let memos = find_memo_from_db(
+            sqlite_pool.clone(),
+            "".to_string(),
+            vec![tags[1].id as i32, tags[2].id as i32],
+        )
+        .await
+        .unwrap();
+        assert_eq!(memos.len(), 3);
+        assert_eq!(memos[0].id, 1);
+        assert_eq!(memos[1].id, 2);
+        assert_eq!(memos[2].id, 4);
     }
 
     #[tokio::test]
     async fn test_テキストタイトルのみでも検索できること() {
         let sqlite_pool = setup_test_db().await;
+        setup_test_data(sqlite_pool.clone()).await;
 
-        create_folder_in_db(sqlite_pool.clone(), "test".to_string())
-            .await
-            .unwrap();
-
-        let memo = CreateMemoIn {
-            title: "test".to_string(),
-            folder_id: Some(1),
-            content: "test".to_string(),
-            tags: Some(vec![1]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo).await.unwrap();
-
-        let memo2 = CreateMemoIn {
-            title: "test2".to_string(),
-            folder_id: Some(1),
-            content: "test2".to_string(),
-            tags: Some(vec![2]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo2).await.unwrap();
-
-        let memo3 = CreateMemoIn {
-            title: "hoge".to_string(),
-            folder_id: Some(1),
-            content: "hoge".to_string(),
-            tags: Some(vec![3]),
-        };
-        create_memo_in_db(sqlite_pool.clone(), memo3).await.unwrap();
-
-        let memos = find_memo_from_db(sqlite_pool.clone(), "test".to_string(), vec![])
+        // tags = []
+        // title = "e"
+        let memos = find_memo_from_db(sqlite_pool.clone(), "e".to_string(), vec![])
             .await
             .unwrap();
         assert_eq!(memos.len(), 2);
+        assert_eq!(memos[0].id, 3);
+        assert_eq!(memos[1].id, 4);
     }
 }
