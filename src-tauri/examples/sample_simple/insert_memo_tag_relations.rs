d6d063a4177b9054c692de16a2abad9d84e9aa14
GASHIRA-E <dsteams@outlook.jp>
2025-01-04 22:59:11 +0900

    tauri add store 実行

--- src-tauri/examples/sample_simple/insert_memo_tag_relations.rs
+++ src-tauri/examples/sample_simple/insert_memo_tag_relations.rs
@@ -1,15 +1,20 @@
 use sqlx::{Pool, Sqlite};
 
-pub async fn insert_memo_tag_relations(database_url: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn insert_memo_tag_relations(
+    database_url: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     println!("insert_memo_tag_relationsへテストデータの挿入を開始します。");
 
     let pool = Pool::<Sqlite>::connect(database_url).await?;
 
     // テストデータの挿入
     sqlx::query("INSERT INTO MemoTagRelations (memo_id, tag_id) VALUES (?, ?), (?, ?), (?, ?)")
-        .bind(1).bind(1)
-        .bind(2).bind(2)
-        .bind(3).bind(3)
+        .bind(1)
+        .bind(1)
+        .bind(2)
+        .bind(2)
+        .bind(3)
+        .bind(3)
         .execute(&pool)
         .await?;
 
