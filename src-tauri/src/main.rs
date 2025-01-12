3adf1bb1c38dec1eb97c27a6e4af950b9e0e4b0c
GASHIRA-E <dsteams@outlook.jp>
2025-01-12 20:20:30 +0900

    権限がなかったので修正

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sqlite_pool = md_memo_light_lib::db_init()?;
-    md_memo_light_lib::run(sqlite_pool)?;
+    println!("start main");
+    // let sqlite_pool = md_memo_light_lib::db_init()?;
+    // md_memo_light_lib::run(sqlite_pool)?;
+    md_memo_light_lib::run()?;
     Ok(())
 }
